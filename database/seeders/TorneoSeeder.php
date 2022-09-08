4b5b8e4ac4ba8621139ec8405695caa34c2e9f8f
Eduardo Hernandez <eddyhb1@hotmail.com>
2022-09-09 00:43:57 +0100

    minor fixes

--- database/seeders/TorneoSeeder.php
+++ database/seeders/TorneoSeeder.php
@@ -23,11 +23,28 @@ class TorneoSeeder extends Seeder
                 "fecha_fin" => "2022-09-30",
                 "fecha_inicio" => "2022-09-20",
                 "fecha_limite" => "2022-09-15",
-                "formato" => 0,
+                "formato" => 1,
                 "max_parejas" => "12",
-                "nombre" => "Prueba",
+                "nombre" => "Waiting",
                 "organizador_id" => "1",
                 "precio" => "12",
+            ]
+        ]);
+        DB::table('torneos')->insert([
+            [
+                "activo" => false,
+                "ciudad" => "Las Palmas de Gran Canaria",
+                "club" => "Club Sánchez",
+                "descripcion" => "Descripción de prueba del torneo",
+                "fecha_fin" => "2022-09-30",
+                "fecha_inicio" => "2022-09-20",
+                "fecha_limite" => "2022-09-15",
+                "formato" => 1,
+                "max_parejas" => "12",
+                "nombre" => "Playing",
+                "organizador_id" => "1",
+                "precio" => "12",
+                "en_juego" => "1"
             ]
         ]);
     }
